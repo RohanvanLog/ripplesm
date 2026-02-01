c4a09f0d16d1dd013469d53f5213b6936dfecaee
rohan <rohanvanlog@gmail.com>
2026-02-01 13:06:57 +0200

    Init commit

--- src/config/nav.js
+++ src/config/nav.js
@@ -4,15 +4,7 @@ export const nav = [
 		slug: '/',
 	},
 	{
-		title: 'Blog',
-		slug: '/blog',
-	},
-	{
-		title: 'About',
-		slug: '/company/about',
-	},
-	{
 		title: 'Contact',
-		slug: '/company/contact',
+		slug: '/contact',
 	},
 ];
